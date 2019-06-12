176b4ea069262b887eee6262a889e6703367e980
selam <selam0315@gmail.com>
2019-06-12 09:05:41 -0500

    a facility plowed field is created

--- src/Models/Facilities/PlowedField.cs
+++ src/Models/Facilities/PlowedField.cs
@@ -5,12 +5,13 @@ using Trestlebridge.Interfaces;
 
 
 namespace Trestlebridge.Models.Facilities {
-    public class PlowedField : IFacility<IGrazing>
+    public class PlowedField : IFacility<ISeedProducing>
     {
         private int _capacity = 50;
         private Guid _id = Guid.NewGuid();
 
-        private List<IGrazing> _animals = new List<IGrazing>();
+        private List<ISeedProducing> _plants = new List<ISeedProducing>();
+        // private List<ICompost> _compostPlants = new List<ICompost>();
 
         public double Capacity {
             get {
@@ -18,13 +19,27 @@ namespace Trestlebridge.Models.Facilities {
             }
         }
 
-        public void AddResource (IGrazing animal)
+        // {
+        //     // TODO: implement this...
+        //     throw new NotImplementedException();
+        // }
+        // public double Compost () {
+        //     return _composted;
+        // }
+        public void AddResource (ISeedProducing plant)
         {
-            // TODO: implement this...
-            throw new NotImplementedException();
+            try
+            {
+                _plants.Add(plant);
+                // _compostPlants.Add(plant);
+            }
+            catch(Exception)
+            {
+                throw new NotImplementedException();
+            }
         }
 
-        public void AddResource (List<IGrazing> animals)
+        public void AddResource (List<ISeedProducing> plants)
         {
             // TODO: implement this...
             throw new NotImplementedException();
@@ -35,8 +50,8 @@ namespace Trestlebridge.Models.Facilities {
             StringBuilder output = new StringBuilder();
             string shortId = $"{this._id.ToString().Substring(this._id.ToString().Length - 6)}";
 
-            output.Append($"Plowed field {shortId} has {this._animals.Count} animals\n");
-            this._animals.ForEach(a => output.Append($"   {a}\n"));
+            output.Append($"Plowed field {shortId} has {this._plants.Count} plants\n");
+            this._plants.ForEach(a => output.Append($"   {a}\n"));
 
             return output.ToString();
         }
